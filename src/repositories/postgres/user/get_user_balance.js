24d8c78a3e137dc7b7442f8437dfcf9165f8d0c9
Mamadu Sama <mamadusama19@gmail.com>
2025-10-25 18:11:23 +0100

    feat: add PostgresGetUserBalanceRepository

---
+++ src/repositories/postgres/user/get_user_balance.js
@@ -0,0 +1,31 @@
+import { PostgresHelper } from '../../../db/postgres/helper.js'
+
+export class PostgresGetUserBalanceRepository {
+    async execute(userId) {
+        const balance = await PostgresHelper.query(
+            `
+            SELECT 
+            SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END)
+            AS earning,
+            SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
+            AS Expenses,
+            SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
+            AS Investiment,
+            (
+            SUM(CASE WHEN type = 'EARNING' THEN amount ELSE 0 END) 
+                - SUM(CASE WHEN type = 'EXPENSE' THEN amount ELSE 0 END)
+                - SUM(CASE WHEN type = 'INVESTMENT' THEN amount ELSE 0 END)
+            
+                
+            ) AS  balance
+            
+
+            FROM transactions
+            WHERE user_id = $1;
+
+            `,
+            [userId],
+        )
+        return balance[0]
+    }
+}
