54980a6e9646921967970e8fd29d879d68b526ed
Mamadu Sama <mamadusama19@gmail.com>
2025-10-25 18:13:19 +0100

    feat: add PostgresGetUserBalanceRepository

--- src/repositories/postgres/user/get_user_balance.js
+++ src/repositories/postgres/user/get_user_balance.js
@@ -26,6 +26,9 @@ export class PostgresGetUserBalanceRepository {
             `,
             [userId],
         )
-        return balance[0]
+        return {
+            userId,
+            ...balance[0],
+        }
     }
 }
