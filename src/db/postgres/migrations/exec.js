db5eef9a21778e96b54fc8cb9e8bed4e10f91c20
Mamadu Sama <mamadusama19@gmail.com>
2025-10-28 19:39:38 +0000

    feat: update execMigrations script to all sql files

--- src/db/postgres/migrations/exec.js
+++ src/db/postgres/migrations/exec.js
@@ -12,10 +12,18 @@ const execMigrations = async () => {
     const client = await pool.connect()
 
     try {
-        const filePath = path.join(__dirname, './01-init.sql')
-        const script = fs.readFileSync(filePath, 'utf-8')
+        const files = fs
+            .readdirSync(__dirname)
+            .filter((file) => file.endsWith('.sql'))
 
-        await client.query(script)
+        for (const file of files) {
+            const filePath = path.join(__dirname, file)
+            const script = fs.readFileSync(filePath, 'utf-8')
+
+            await client.query(script)
+            console.log(`Executed migration for file: ${file}`)
+        }
+
         console.log('migrations was execcuted successfull')
     } catch (error) {
         console.log(error)
