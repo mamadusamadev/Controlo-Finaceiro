703e77820bd435b3a99377fd5b36398d0a49fb2c
Mamadu Sama <mamadusama19@gmail.com>
2025-10-30 22:04:11 +0000

    feat: ensure add createuserRepository and get_user_by_idRepository use prisma

--- src/repositories/postgres/user/create_user.js
+++ src/repositories/postgres/user/create_user.js
@@ -1,18 +1,15 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
-
+import { prisma } from '../../../../prisma/prisma.js'
 export class PostgresCreateUserRepository {
     async execute(createUserParams) {
-        // create user in postgres
-        const results = await PostgresHelper.query(
-            'INSERT INTO users (id, first_name, last_name,  email, password) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [
-                createUserParams.id,
-                createUserParams.first_name,
-                createUserParams.last_name,
-                createUserParams.email,
-                createUserParams.password,
-            ],
-        )
-        return results[0]
+        const user = await prisma.user.create({
+            data: {
+                id: createUserParams.id,
+                first_name: createUserParams.first_name,
+                last_name: createUserParams.last_name,
+                email: createUserParams.email,
+                password: createUserParams.password,
+            },
+        })
+        return user
     }
 }
