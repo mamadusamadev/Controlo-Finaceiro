876beba845a27f9f78a6235e409fb56718cae3cc
Mamadu Sama <mamadusama19@gmail.com>
2025-10-31 19:56:24 +0000

    feat: remove   migration : node  src/db/postgres/migrations/exec.js

--- src/repositories/postgres/transaction/create_transaction.js
+++ src/repositories/postgres/transaction/create_transaction.js
@@ -3,9 +3,7 @@ import { prisma } from '../../../../prisma/prisma.js'
 export class PostgresCreateTransactionRepository {
     async execute(createTransactionParams) {
         const createTransaction = await prisma.transaction.create({
-            data: {
-                id: createTransactionParams,
-            },
+            data: createTransactionParams,
         })
         return createTransaction
     }
